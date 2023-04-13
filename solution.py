81c70f2a8d6191469817fe9cd781cfe6b41c097d
Yulia Izmailova <104653247+alepouzs@users.noreply.github.com>
2023-04-13 03:11:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from hyppo.ksample import MMD
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 487382438 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, pvalue = MMD().test(x, y)
+    stat, pvalue = cramervonmises_2samp(x, y)
     return pvalue < 0.06
